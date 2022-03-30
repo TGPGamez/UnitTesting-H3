9673644ba01b1d188b80adbf2391cdaa0699b795
TGPGamez <58465104+TGPGamez@users.noreply.github.com>
2022-03-30 19:18:52 +0200

    Code comment

--- ATM-Library/Logic/ATM-Machine.cs
+++ ATM-Library/Logic/ATM-Machine.cs
@@ -8,29 +8,50 @@ namespace ATM_Library
 {
     public class ATM_Machine
     {
+        /// <summary>
+        /// Check if the card pincode is the entered code
+        /// </summary>
+        /// <param name="card_Pincode"></param>
+        /// <param name="compare_Pincode"></param>
+        /// <returns></returns>
         public bool CheckCardPincode(string card_Pincode, string compare_Pincode)
         {
+            //Check if card pincode is null or whitespace only
             if (string.IsNullOrWhiteSpace(card_Pincode))
             {
                 return false;
             }
+            // Check if entered pincode is null or whitespace only
             if (string.IsNullOrWhiteSpace(compare_Pincode))
             {
                 return false;
             }
+            // Compare the two strings
             return card_Pincode.Equals(compare_Pincode);
         }
 
+        /// <summary>
+        /// Deposit money to an card account
+        /// </summary>
+        /// <param name="card"></param>
+        /// <param name="amount"></param>
         public void DepositToAccount(Card card, double amount)
         {
+            //Check if amount is not under 0
             if (amount > 0)
             {
                 card.Linked_Account.Deposit(amount);
             }
         }
 
+        /// <summary>
+        /// Withdraw money from an card account
+        /// </summary>
+        /// <param name="card"></param>
+        /// <param name="amount"></param>
         public void WithdrawFromAccount(Card card, double amount)
         {
+            //Check if amount is not under 0
             if (amount > 0)
             {
                 if (card.Linked_Account.Balance >= amount)
